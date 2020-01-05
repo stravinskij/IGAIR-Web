aaba9376782b93f0dd220fa498dd84659ff384cd
Unikore <captain.unikore@gmail.com>
2020-01-05 19:23:10 +0100

    fix eslint warnings about preferring arrow syntax

--- functions/routes/routerMain.js
+++ functions/routes/routerMain.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
     res.set('Cache-control', 'public, max-age=300, s-maxage=600');
     res.render('home.hbs', {title: 'Intergalactic Airlines'});
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', (req, res, next) => {
     res.set('Cache-control', 'public, max-age=300, s-maxage=600');
     res.render('about.hbs', {title: 'About IGAIR'});
 });
 
-router.get('/structure', function(req, res, next) {
+router.get('/structure', (req, res, next) => {
     res.set('Cache-control', 'public, max-age=300, s-maxage=600');
     res.render('structure.hbs', {title: 'IGAIR Structure'});
 });
 
-router.get('/store', function(req, res, next) {
+router.get('/store', (req, res, next) => {
     res.set('Cache-control', 'public, max-age=300, s-maxage=600');
     res.render('store.hbs', {title: 'IGAIR Store'});
 });
 
-router.get('404', function(req, res, next) {
+router.get('404', (req, res, next) => {
     res.set('Cache-Control', 'public, max-age=300, s-maxage=3600');
     res.render('404.hbs');
 });
--- functions/routes/routerNews.js
+++ functions/routes/routerNews.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/news', function(req, res, next) {
+router.get('/news', (req, res, next) => {
     res.set('Cache-control', 'public, max-age=300, s-maxage=600');
     res.render('news.hbs', {newsArticleList});
 });
 
-router.get('news/:id', function(req, res, next) {
+router.get('news/:id', (req, res, next) => {
     res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
     res.render('newsArticle.hbs', {articleTitle, articleContent, commentObject});
 });
 
-router.get('news/:id/edit', function(req,res,next) {
+router.get('news/:id/edit', (req, res, next) => {
     // TODO: check to see if permissions to edit are OK first
     res.render('newsArticleEditor.hbs', {articleTitle, articleContent});
 });
 
-// router.post('news/:id/edit', function(req, res, next) {
+// router.post('news/:id/edit', (req, res, next) => {
 //     // TODO: Check to see if permissions to edit are OK first
 // });
 
