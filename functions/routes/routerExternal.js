fe221d34ae33a3cd585ce01d9115981b57a7f1ad
JACOB PATTERSON <jacobpatterson0@gmail.com>
2020-01-08 07:01:38 -0600

    actually send a status code on a /ext/ redirect

--- functions/routes/routerExternal.js
+++ functions/routes/routerExternal.js
@@ -6,13 +6,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/org', function(req, res, next) {
-    res.redirect("https://robertsspaceindustries.com/orgs/IGAIR");
+    res.redirect(200,"https://robertsspaceindustries.com/orgs/IGAIR");
 });
 
 router.get('/discord', function(req,res,next) {
-    res.redirect("https://discord.gg/zRyvbAy");
+    res.redirect(200,"https://discord.gg/zRyvbAy");
 });
 
 router.get('/spectrum', function(req,res,next) {
-    res.redirect("https://robertsspaceindustries.com/spectrum/community/IGAIR");
+    res.redirect(200,"https://robertsspaceindustries.com/spectrum/community/IGAIR");
 });
